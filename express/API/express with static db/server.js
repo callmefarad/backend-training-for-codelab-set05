9ccb069a83fd2759e6695c712c7dad58e4f537fc
callmefarad <ubani.udochukwu@gmail.com>
2022-02-11 18:16:44 +0100

    finish the simple API

--- express/API/express with static db/server.js
+++ express/API/express with static db/server.js
@@ -3,8 +3,9 @@ const port = 2023
 
 
 const app = express()
+app.use(express.json())
 
-// create a JavaScript array of object that stands as our database
+// create a JavaScript array of object (to be used as dummy database)
 const studentInfo = [
     {
     id: 1,
@@ -151,17 +152,19 @@ const studentInfo = [
 // all request goes here
 // Welcome message
 app.get('/', (req, res)=>{
-    res.json({message: "Welcome to EXPRESS API"})
+    // return a response to the client
+    res.json({message: "Welcome to EXPRESS API WITH DUMMY DATA"})
 })
 
 // get all students
 app.get('/studentInfo', (req, res)=>{
     try{
-        // check if there is content in the database
+        // check if there is content in the array
         if(studentInfo.length < 1){
+            // return an error message
             res.json({message: "No content in the database"})
         }else{
-            // get all the students in that database
+            // get all the students in that array and send a response to the client
             res.status(200).json({message: "All student of CodeLab", data: studentInfo})
         }
     }catch(error){
@@ -173,11 +176,15 @@ app.get('/studentInfo', (req, res)=>{
 app.get('/studentInfo/:id', (req, res) => {
     try{
         // get the id
-        const id = req.params.id
+        const id = parseInt(req.params.id)
+        // get the object of the id
+        const student = studentInfo.find((stud)=>stud.id === id)
+        // validate the id
         if(!id){
+            // throw an error message
             res.json({message: `This id: ${req.params.id} does not exist`})
         }else{
-            const student = studentInfo.find((stud)=>stud.id === id)
+            // return a response to the client
             res.status(200).json({message: `Student with id: ${req.params.id} was found`, data: student})
         }
     }catch(error){
@@ -186,10 +193,81 @@ app.get('/studentInfo/:id', (req, res) => {
 })
 
 // create/add a student
+app.post('/studentInfo', (req, res) => {
+    try{
+        // create a new object
+        const newStudent = {
+            id: studentInfo.length + 1,
+            duration: req.body.duration,
+            course: req.body.course,
+            duration: req.body.duration,
+            institution: req.body.institution,
+            grade: {
+                javascript: req.body.grade.javascript,
+                HTML: req.body.grade.HTML,
+                CSS: req.body.grade.CSS,
+                React: req.body.grade.React,
+                Node: req.body.grade.Node
+            }
+        }
+
+        // add the new object to the array
+        studentInfo.push(newStudent)
+        // send a response to the client
+        res.status(200).json({message: "Student Created Successfully", data: newStudent})
+    }catch(error){
+        console.log(error.message)
+        res.json({message: error.message})
+    }
+})
 
 // update a student
+app.patch('/studentInfo/:id', (req, res) => {
+    try{
+        // get the id passed to the url
+        const id = parseInt(req.params.id)
+        // get the object of the id
+        const studentId = studentInfo.find((student)=>student.id === id)
+        // update the fields of the student with the id found in the array
+        studentId.name = req.body.name,
+        studentId.course = req.body.course,
+        studentId.duration = req.body.duration,
+        studentId.institution = req.body.institution,
+        studentId.javascript = req.body.javascript,
+        studentId.HTML = req.body.duration,
+        studentId.CSS = req.body.duration,
+        studentId.React = req.body.duration,
+        studentId.Node = req.body.duration
+
+        // return the updated object to the client
+        res.status(200).json({message: "Updated Successfully", data: studentId})
+    }catch(error){
+        console.log(error.message)
+    }
+})
 
 // delete a student
+app.delete('/studentInfo/:id', (req, res) => {
+    try{
+        // get the id 
+        const id = parseInt(req.params.id)
+        // get the object of the id passed to the url
+        const studentId = studentInfo.find((student)=>student.id === id)
+        // validate the id
+        if(!id){
+            res.json({message: `Invalid id : ${req.params.id}`})
+        }else{
+            // get the index of the id
+            const index = studentInfo.indexOf(studentId)
+            // remove the object of the index found
+            studentInfo.splice(index, 1)
+            // return a  response to the client
+            res.status(200).json({message: `Student with id: ${req.params.id} was deleted successfully`})
+        }
+    }catch(error){
+        console.log(error.message)
+    }
+})
 
 
 // all request ends here
