8605ef6862902769635d74181699be94505b94a9
callmefarad <ubani.udochukwu@gmail.com>
2022-03-14 13:01:01 +0100

    added procfile

--- express/API/node with express multer cloudinarya and mongodb/config/blogdb.js
+++ express/API/node with express multer cloudinarya and mongodb/config/blogdb.js
@@ -1,7 +1,8 @@
 require( 'dotenv' ).config();
 const mongoose = require( 'mongoose' )
 
-url = process.env.LOCAL_DB
+// url = process.env.LOCAL_DB
+url = process.env.MONGODB_URI
 mongoose.connect( url )
     .then( () => {
         console.log( 'Database connection established' )
